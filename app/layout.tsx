8c15b5f7c6dd1bc04b232159e2c3bb6d612a49d6
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-11-08 20:20:20 +0600

    upto login

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+// import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+export const metadata = {
+  title: "AirBnb",
+  description: "Airlines Bokking Solution",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          {/* <Modal actionLabel="Submit" title="Hello World" isOpen /> */}
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
